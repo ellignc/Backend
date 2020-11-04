9805b095a21c7d7a3806aa9e1a6a1150b71ccd9b
Elli <egcabusay14@gmail.com>
2020-11-04 17:51:31 +0800

    refactor: put app initialization to app.js

---
+++ app.js
@@ -0,0 +1,30 @@
+const fastify =  require('fastify');
+
+/**
+ *  This is the function to call to initialize the server
+ * 
+ *  @param {{ logger: boolean, trustProxy: boolean }} opts
+ *  @returns {*}
+ */
+exports.build = async (opts = {logger: true, trustProxy: true}) => {
+    // initializes our server using Fastify
+    const app = fastify(opts);
+
+    // access root address - http://localhost/
+    app.get('/', {
+        // object
+
+        /**
+         *  @param {*} req - this is the request parameter that is sent by the client
+         */
+        handler: async (req) => {
+            console.log('Hello World!!!');
+
+            // this is the response in JSON
+            return {success: true}
+        }
+    });
+
+    return app;
+
+};
--- index.js
+++ index.js
@@ -1,34 +1,21 @@
-const Fastify =  require('fastify');
 
-// initialize our server using Fastify
-const server = Fastify({
-    logger: true,
-    trustProxy: true
-});
 
-// access root address - http://localhost/
-server.get('/', {
-    // object
+const {build} = require('./app');
 
-    /**
-     *  @param {*} req - this is the request parameter that is sent by the client
-     */
-    handler: async (req) => {
-        console.log('Hello World!!!');
-
-        // this is the response in JSON
-        return {success: true}
-    }
-});
-
+/**
+ * starts the application
+ */
 async function start () {
+    // calls to build an app
+    const app = await build();
+
     // get  the port from environment variable
     // if this is the command export PORT=8000 && node index.js 
     // then PORT=8000 else default is 8080
     const port = parseInt(process.env.PORT || '8080');
     const address = '0.0.0.0';
 
-    const addr = await server.listen(port, address);
+    const addr = await app.listen(port, address);
     console.log(`Listening on ${addr}`);
 }
 
