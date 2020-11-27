7ced9425781398090217ef7efee8aaedb03d617a
ellignc <encabusay@up.edu.ph>
2020-11-27 22:18:31 +0800

    feat: add the ability to get one todo

---
+++ routes/todo/get.js
@@ -0,0 +1,33 @@
+const { getTodos } = require('../../lib/get-todos');
+const { join } = require('path');
+
+/**
+ *  Gets one todos
+ * 
+ *  @param {*} app
+ */
+exports.get = app => {
+
+    /**
+     *  This gets one todos from the database given a unique ID
+     * 
+     *  @param {import('fastify').FastifyRequest} request
+     */
+    app.get('/todo/:id', (request) => {
+        const { params } = request;
+        const { id } = params;
+
+        const encoding = 'utf8';
+        const filename = join(__dirname, '../../database.json');
+        const todos = getTodos(filename, encoding);
+        
+        const index = todos.findIndex(todo => todo.id === id);
+
+        const data = todos[index];
+        
+        return {
+            success: true,
+            data
+        };
+    });
+};
